f7a03b16af23b68b23083e96a9349a9f2d37e722
Gary <gary@elasticdev.io>
2020-01-03 17:15:02 +0800

    update

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -10,15 +10,7 @@ fi
 RET=1
 while [[ RET -ne 0 ]]; do
     echo "=> Waiting for confirmation of MongoDB service startup"
-    sleep 5
-    mongo --ssl admin --sslAllowInvalidCertificates --eval "help" > /dev/null 2>&1
-    RET=$?
-done
-
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MongoDB service startup"
-    sleep 5
+    sleep 1
     mongo --ssl admin --sslAllowInvalidCertificates --eval "help" > /dev/null 2>&1
     RET=$?
 done
