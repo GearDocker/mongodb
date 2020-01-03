180c7b4f9b58d39f09df9d078133011819d8637b
Gary <gary@elasticdev.io>
2020-01-03 16:52:05 +0800

    update

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -23,13 +23,13 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-echo "=> Creating an admin user with a $MONGO_PASSWD password in MongoDB"
-mongo admin --ssl --sslAllowInvalidCertificates --eval "db.createUser({user: 'admin', pwd: '$MONGO_PASSWD', roles:[{role:'root',db:'admin'}]});"
+echo "=> Creating an admin user with a $MONGODB_PASS password in MongoDB"
+mongo admin --ssl --sslAllowInvalidCertificates --eval "db.createUser({user: 'admin', pwd: '$MONGODB_PASS', roles:[{role:'root',db:'admin'}]});"
 
 echo "========================================================================"
 echo "You can now connect to this MongoDB server using:"
 echo ""
-echo "    mongo admin --sslAllowInvalidCertificates --ssl -u admin -p $MONGO_PASSWD --host <host> --port <port>"
+echo "    mongo admin --sslAllowInvalidCertificates --ssl -u admin -p $MONGODB_PASS --host <host> --port <port>"
 echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "========================================================================"
